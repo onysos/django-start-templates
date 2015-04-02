6a81f1828ba78fdefe8ed79ccf376be9336a475e
darius BERNARD <darius.bernard@onysos.fr>
2015-04-02 09:14:08 +0200

    correction in the setup.py files to take into account the templates and manages files.

--- templates/generic__app_template/setup.py
+++ templates/generic__app_template/setup.py
@@ -5,6 +5,45 @@ import os
 sys.path[0:0] = [os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")]
 
 
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, package_data = [], {}
+
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk("onysos_kolab"):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
+    if '__init__.py' in filenames:
+        packages.append(package_name)
+    elif filenames:
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.append(parts.pop())
+        relative_path.reverse()
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
 setup(
     name="{{project_name}}",
     version=__import__("{{project_name}}").__version__,
@@ -12,7 +51,8 @@ setup(
     license='',
     description='',
     author='!!AUTHOR!!',
-    packages=find_packages('src'),
+    packages=packages,
+    package_data=package_data,
     package_dir={'': 'src'},
     install_requires=['setuptools'],
 )
